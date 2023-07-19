263681b21be55cc5a97012e48cf2f42b68161cb1
c0nD <colintiller21@gmail.com>
2023-07-19 02:48:48 -0400

    compares frames to template image to save frames close enough

--- src/video_processor.py
+++ src/video_processor.py
@@ -1,5 +1,6 @@
 import cv2
 import image_preprocessor as ipp
+import os
 
 def process_video(video_path, output_path):
     video = cv2.VideoCapture(video_path)
@@ -31,29 +32,48 @@ def process_video(video_path, output_path):
 def compare_to_template(video_path, template_path):
     video = cv2.VideoCapture(video_path)
     template = cv2.imread(template_path, cv2.IMREAD_GRAYSCALE)
+
+    # Check if the template loaded correctly
     if template is None:
         raise Exception(f"Failed to load template image from path: {template_path}")
 
     template_h, template_w = template.shape
+
+    # Create directory if it doesn't exist
+    output_dir = 'CreampuffBOT/temp/vid_imgs'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    else:
+        # Clear the directory by deleting all files in it
+        for file_name in os.listdir(output_dir):
+            file_path = os.path.join(output_dir, file_name)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+            except Exception as e:
+                print(e)
 
     frame_number = 0
     while True:
         ret, frame = video.read()
         if not ret:
             break
-
-        # Convert the frame to grayscale
+        
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        res = cv2.matchTemplate(gray_frame, template, cv2.TM_CCOEFF_NORMED)
+        _, max_val, _, _ = cv2.minMaxLoc(res)
 
-        # Match template and get the maximum value as the score
-        result = cv2.matchTemplate(gray_frame, template, cv2.TM_CCOEFF_NORMED)
-        _, max_val, _, _ = cv2.minMaxLoc(result)
-
-        print(f"Frame {frame_number} has a match score of {max_val:.4f}")
+        if max_val > 0.3934:
+            output_path = os.path.join(output_dir, f'frame_{frame_number}.png')
+            cv2.imwrite(output_path, frame)
+        
         frame_number += 1
 
     video.release()
-
+    
+    
+    
+    
 
 
 process_video('CreampuffBOT/temp/test_video.mp4', 'CreampuffBOT/temp/test_out.mp4')
